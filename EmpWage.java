42ba65ceec13908deeaea2020715f40d3a08cb85
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-28 17:06:22 +0530

    UC-5: calculated wage for month

--- EmpWage.java
+++ EmpWage.java
@@ -6,8 +6,8 @@ public class EmpWage
 		System.out.println("====Welcome to Employee Wage Problem=======");
 		System.out.println("===========================================");
 
-		int present = 1;
-		int salary = 0,wagePerHrs = 20,fullDayHrs = 8;
+		int present = 1,wage = 0;
+                int salary = 0,wagePerHrs = 20,fullDayHrs = 8;
 
                 int empCheck = (int)Math.floor(Math.random() * 10) % 2;
 
@@ -15,31 +15,45 @@ public class EmpWage
                 {
                         System.out.println("Employee is present");
 
-                        salary = wagePerHrs * fullDayHrs;
+                        wage = wagePerHrs * fullDayHrs;
 
-                        System.out.println("Daily Employee Wage salary = "+salary);
+                        System.out.println("Daily Employee Wage salary = "+wage);
                 }
 
         //======================================================================================
 
-                int isPart = 0, isFull = 1, totalSalary = 0, empRate = 10;
-                int empHrs=0;
+                int isPart = 0, isFull = 1, totalSalary = 0, empRate = 20;
+                int empHrs=0,totalDays = 20;
 
-                switch(empCheck)
+                for(int day=1; day<=totalDays;day++)
                 {
-                        case 1:
-                                empHrs = 16;
-                                System.out.println("Working full time....");
-                                break;
-                        case 0:
-                                empHrs = 8;
-                                System.out.println("Working part time....");
-                                break;
-                        default:
-                                System.out.println("Employee is absent....");
-                                break;
+                        switch(empCheck)
+                        {
+                                case 1:
+                                        empHrs = 16;
+                                        break;
+                                case 0:
+                                        empHrs = 8;
+                                        break;
+                                default:
+                                        empHrs = 0;
+                                        break;
+                        }
+
+                        salary = empHrs * empRate;
+                        totalSalary = totalSalary + salary + wage;
                 }
-                totalSalary = empHrs * empRate;
-                System.out.println("Total Salary = "+totalSalary);
+
+                if(empCheck == 0)
+                {
+                        System.out.println("Working part time...");
+                        System.out.println("Total Salary = "+totalSalary);
+                }
+                else
+                {
+                        System.out.println("Working full time...");
+                        System.out.println("Total Salary = "+totalSalary);
+
+                }
 	}
 }
