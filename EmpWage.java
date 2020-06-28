14a0d3ba00959849a210416709f4c50347e817a2
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-28 16:53:21 +0530

    UC-2 : Daily wage added

--- EmpWage.java
+++ EmpWage.java
@@ -7,16 +7,19 @@ public class EmpWage
 		System.out.println("===========================================");
 
 		int present = 1;
+		int salary = 0,wagePerHrs = 20,fullDayHrs = 8;
 
                 double empCheck = Math.floor(Math.random() * 10) % 2;
 
                 if(empCheck == present)
                 {
                         System.out.println("Employee is present");
+			salary = wagePerHrs * fullDayHrs;
                 }
                 else
                 {
                         System.out.println("Employee is not present");
                 }
+		System.out.println("Daily Employee Wage salary = "+salary);
 	}
 }
