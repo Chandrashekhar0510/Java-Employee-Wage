1edf20f6a5301d49a4faf3d90bf81deaf788f420
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-28 16:49:21 +0530

    UC-1 : Attendance checked

--- EmpWage.java
+++ EmpWage.java
@@ -5,5 +5,18 @@ public class EmpWage
 		System.out.println("===========================================");
 		System.out.println("====Welcome to Employee Wage Problem=======");
 		System.out.println("===========================================");
+
+		int present = 1;
+
+                double empCheck = Math.floor(Math.random() * 10) % 2;
+
+                if(empCheck == present)
+                {
+                        System.out.println("Employee is present");
+                }
+                else
+                {
+                        System.out.println("Employee is not present");
+                }
 	}
 }
