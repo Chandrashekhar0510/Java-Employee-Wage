42f5d43683ac1fc2dc67652470498e7f3bb0997d
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-28 22:28:06 +0530

    UC-12: Refactor code by using linked list

--- EmpWage.java
+++ EmpWage.java
@@ -1,13 +1,16 @@
+import java.util.*;
+
 public class EmpWage implements IComputeWage
 {
         public static final int isPart = 1, isFull = 2;
         int numOfCompany = 0;
 
-        CompanyWage[] wageArray;
-
+        LinkedList<CompanyWage> companyWageList;
+	Map<String,CompanyWage> companyWageMap;
         public EmpWage()
         {
-                wageArray = new CompanyWage[10];
+                companyWageList = new LinkedList<>();
+		companyWageMap = new HashMap<>();
         }
 
         public static void main(String[] args)
@@ -21,25 +24,34 @@ public class EmpWage implements IComputeWage
                 empWage.addCompanyWage("CapG", 30, 25, 100);
                 empWage.addCompanyWage("BridgeLabz", 20, 20, 120);
                 empWage.computeEmpWage();
+
+		System.out.println("Total Wage for CapGemini => "+empWage.getTotalWage("CapG"));
         }
 
 
         public void addCompanyWage(String company, int empRatePerHr, int workingDays, int maxHrsPerMonth)
         {
-                wageArray[numOfCompany] = new CompanyWage(company, empRatePerHr, workingDays, maxHrsPerMonth);
+                CompanyWage companyWage = new CompanyWage(company, empRatePerHr, workingDays, maxHrsPerMonth);
 
-                numOfCompany++;
+		companyWageList.add(companyWage);
+		companyWageMap.put(company, companyWage);
         }
 
         public void computeEmpWage()
         {
-                for (int i=0; i<numOfCompany; i++)
+                for (int i=0; i<companyWageList.size(); i++)
                 {
-                        wageArray[i].setTotalEmpWage(this.computeEmpWage(wageArray[i]));
+                        CompanyWage companyWage = companyWageList.get(i);
+			companyWage.setTotalEmpWage(this.computeEmpWage(companyWage));
 
-                        System.out.println(wageArray[i]);
+                        System.out.println(companyWage);
                 }
         }
+
+	public int getTotalWage(String company)
+	{
+		return companyWageMap.get(company).totalEmpWage;
+	}
 
         int computeEmpWage(CompanyWage compWage)
         {
--- IComputeWage.java
+++ IComputeWage.java
@@ -2,4 +2,5 @@ public interface IComputeWage
 {
 	public void addCompanyWage(String company, int empRatePerHr, int workingDays, int maxHrsPerMonth);
 	public void computeEmpWage();
+	public int getTotalWage(String company);
 }
